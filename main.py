1975484ab64f5168f964bdbac4e6f204aee70ea2
Elvir Misini <elvirmisini@gmail.com>
2023-12-21 08:57:05 +0100

    2 more instances

--- main.py
+++ main.py
@@ -42,9 +42,10 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--version', type=str, required=True)
 
     args = parser.parse_args()
-    instancat=["I1662_S10000_C1000.txt","I2000_S12000_C57.txt","I2000_S6000_C376.txt","I2500_S10000_C306.txt","I2999_S17994_C103.txt","I3000_S12000_C407.txt","I3000_S15000_C316.txt","I3000_S18000_C227.txt","I3333_S13332_C428.txt","I4000_S12000_C161.txt","I500_S2000_C315.txt","I600_S3000_C332.txt","I70_S210_C200.txt","I80_S240_C300.txt","I80_S480_C600.txt","I90_S360_C400.txt"]
+#    instancat=["I1662_S10000_C1000.txt","I2000_S12000_C57.txt","I2000_S6000_C376.txt","I2500_S10000_C306.txt","I2999_S17994_C103.txt","I3000_S12000_C407.txt","I3000_S15000_C316.txt","I3000_S18000_C227.txt","I3333_S13332_C428.txt","I4000_S12000_C161.txt","I500_S2000_C315.txt","I600_S3000_C332.txt","I70_S210_C200.txt","I80_S240_C300.txt","I80_S480_C600.txt","I90_S360_C400.txt"]
 #    instancat=["I155_S57155_C300.txt","I180_S1080_C800.txt","I200_S1000_C400.txt","I200_S1000_C550.txt","I200_S4200_C400.txt","I200_S17200_C1000.txt","I220_S660_C430.txt","I220_S1100_C558.txt","I241_S43241_C158.txt","I300_S900_C500.txt","I300_S1500_C469.txt","I400_S2400_C944.txt","I444_S1776_C666.txt","I500_S998_C1000.txt"]
    # instancat = ["I200_S1000_C400.txt", "I200_S1000_C550.txt", "I200_S17200_C1000.txt", "I200_S4200_C400.txt", "I220_S1100_C558.txt", "I241_S43241_C158.txt", "I2500_S10000_C306.txt"]
+    instancat=["I9000_S36000_C1500.txt","I10000_S30000_C1200.txt","I12000_S36000_C2000.txt"]
     for instanca in instancat:
         for run_number in range(1, 11):
                 version = f"{args.version}_{run_number}"
