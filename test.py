f1c40969ac3cb22702a56d8d4a4a0b69dcc80ab5
elvirmisini <elvirmisini@gmail.com>
2024-02-20 08:26:48 +0100

    small change

--- test.py
+++ test.py
@@ -14,10 +14,15 @@ intersections = len(parsed_data['intersections'])
 streets = simulation['streets']
 cars = simulation['cars']
 bonus = simulation['bonus']
+duration_to_pass_through_an_intersection = simulation['duration_to_pass_through_an_intersection']
+limit_on_minimum_cycle_length = simulation['limit_on_minimum_cycle_length']
+limit_on_maximum_cycle_length = simulation['limit_on_maximum_cycle_length']
+limit_on_minimum_green_phase_duration = simulation['limit_on_minimum_green_phase_duration']
+limit_on_maximum_green_phase_duration = simulation['limit_on_maximum_green_phase_duration']
 
 # Write simulation data to txt file
 with open("output.txt", "w") as txt_file:
-    txt_file.write(f"{duration} {intersections} {streets} {cars} {bonus}\n")
+    txt_file.write(f"{duration} {intersections} {streets} {cars} {bonus} {duration_to_pass_through_an_intersection} {limit_on_minimum_cycle_length} {limit_on_maximum_cycle_length} {limit_on_minimum_green_phase_duration} {limit_on_maximum_green_phase_duration}\n")
     
     # Write street data to txt file
     for street in parsed_data['streets']:
@@ -27,3 +32,6 @@ with open("output.txt", "w") as txt_file:
     for car in parsed_data['cars']:
         path = ' '.join(car['path'])
         txt_file.write(f"{len(car['path'])} {path}\n")
+        
+    for intersections in parsed_data['intersections']:
+        txt_file.write(f"{intersections['name']} {intersections['pedestrian_phase_interval']} {intersections['all_red_phase_interval']}\n")
