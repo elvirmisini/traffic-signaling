d6893f4554530cf8f8d2b053ff0469e5ed0c3725
Uran Lajci <uranlajci@hotmail.com>
2024-06-22 10:42:26 +0200

    added new changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fitness_function import fitness_score
 from initial_solution import traffic_based_initial_solution, usage_based_initial_solution
 from input_parser import read_input
 from ouput_writer import save_schedule_to_file
-from parameter_calibration.parameter_1 import optimize_solution_with_ils
+from parameter_calibration.parameter_2 import optimize_solution_with_ils
 
 
 def main(instance_name, variant, version) -> None:
--- ouput_writer.py
+++ ouput_writer.py
@@ -1,6 +1,6 @@
 import os.path
 
-SOLUTION_REPORTER_DIR = 'output'
+SOLUTION_REPORTER_DIR = 'output_parametri_1'
 
 
 def save_schedule_to_file(schedules, streets, filename) -> None:
