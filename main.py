9561b6f81546ed82b2a74de72bc34de0ca653877
Uran Lajci <uranlajci@hotmail.com>
2024-06-24 12:03:54 +0200

    new data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from fitness_function import fitness_score
 from initial_solution import traffic_based_initial_solution, usage_based_initial_solution
 from input_parser import read_input
 from ouput_writer import save_schedule_to_file
-from variantet.ILS_GCGT_GSS_SNS_CGTWC import optimize_solution_with_ils
-# from variantet.ILS_GCGT_RCGT_SNS_CGTWC import optimize_solution_with_ils
+from variantet.ILS_GCGT_RCGT_SNS_CGTWC import optimize_solution_with_ils
+
 # from variantet.ILS_GCGT_GSS_RCGT_SNS import optimize_solution_with_ils
 # from variantet.ILS_GSS_RCGT_SNS_CGTWC import optimize_solution_with_ils
 
