e27d5cc43d60d48a7293ef78cb9bafeff4cc7b73
Elvir Misini <elvirmisini@gmail.com>
2024-06-26 19:55:30 +0200

    Update run_instances.sh

--- run_instances.sh
+++ run_instances.sh
@@ -5,19 +5,19 @@ run_instance() {
     nohup python3 experiment.py $instance_name &
 }
 
-input_dir="./input"
+input_dir="./input1"
 
 pids=()
 
 for file in $input_dir/*
 do
     filename=$(basename "$file")
-    for version in {1..10}
+    for version in {1..2}
     do
         run_instance $filename 
         pids+=($!)
 
-        if [ ${#pids[@]} -ge 50 ]; then
+        if [ ${#pids[@]} -ge 10 ]; then
 
             for pid in "${pids[@]}"; do
                 wait $pid
